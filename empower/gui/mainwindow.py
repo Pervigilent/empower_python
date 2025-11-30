fdb7311b3811a1409e9252d3c88f884e2753d2ff
pervigilent <pervigilent@gmail.com>
2025-11-30 07:25:04 -0700

    Added draw menu for Empower.

--- empower/gui/mainwindow.py
+++ empower/gui/mainwindow.py
@@ -16,6 +16,7 @@ class MainWindow(tk.Tk):
 		self.geometry("1080x720")
 		self.window_mode = WindowMode.BASE
 		self.create_menu()
+		self.create_windows()
 
 	def create_menu(self):
 		self.menubar = tk.Menu(self)
@@ -123,7 +124,50 @@ class MainWindow(tk.Tk):
 		self.empower_project_menu.add_command(label='Project Variables...', command=self.generic_callback)
 		self.empower_project_menu.add_command(label='Datasets...', command=self.generic_callback)
 		self.empower_project_menu.add_command(label='Event Callbacks...', command=self.generic_callback)
-		self.empower_menubar.add_cascade(label='Project', menu=self.empower_project_menu, underline=0)	
+		self.empower_menubar.add_cascade(label='Project', menu=self.empower_project_menu, underline=0)
+		
+		# Empower Draw Menu
+		self.empower_draw_menu = tk.Menu(self.empower_menubar, tearoff=0)
+		self.empower_arc_menu = tk.Menu(self.empower_draw_menu, tearoff=0)
+		self.empower_sweep_menu = tk.Menu(self.empower_draw_menu, tearoff=0)
+		self.empower_userdefinedprimitive_menu = tk.Menu(self.empower_draw_menu, tearoff=0)
+		self.empower_userdefinedmodel_menu = tk.Menu(self.empower_draw_menu, tearoff=0)
+		self.empower_3dcomponentlibrary_menu = tk.Menu(self.empower_draw_menu, tearoff=0)
+		self.empower_linesegment_menu = tk.Menu(self.empower_draw_menu, tearoff=0)
+		
+		self.empower_draw_menu.add_command(label='Line', command=self.generic_callback)
+		self.empower_draw_menu.add_command(label='Spline', command=self.generic_callback)
+		self.empower_draw_menu.add_cascade(label='Arc', menu=self.empower_arc_menu)
+		self.empower_draw_menu.add_command(label='Equation Based Curve', command=self.generic_callback)
+		self.empower_draw_menu.add_separator()
+		self.empower_draw_menu.add_command(label='Rectangle', command=self.generic_callback)
+		self.empower_draw_menu.add_command(label='Ellipse', command=self.generic_callback)
+		self.empower_draw_menu.add_command(label='Circle', command=self.generic_callback)
+		self.empower_draw_menu.add_command(label='Regular Polygon', command=self.generic_callback)
+		self.empower_draw_menu.add_command(label='Equation Based Surface', command=self.generic_callback)
+		self.empower_draw_menu.add_separator()
+		self.empower_draw_menu.add_command(label='Box', command=self.generic_callback)
+		self.empower_draw_menu.add_command(label='Cylinder', command=self.generic_callback)
+		self.empower_draw_menu.add_command(label='Sphere', command=self.generic_callback)
+		self.empower_draw_menu.add_command(label='Torus', command=self.generic_callback)
+		self.empower_draw_menu.add_command(label='Helix', command=self.generic_callback)
+		self.empower_draw_menu.add_command(label='Spiral', command=self.generic_callback)
+		self.empower_draw_menu.add_command(label='Bondwire', command=self.generic_callback)
+		self.empower_draw_menu.add_separator()
+		self.empower_draw_menu.add_command(label='Sweep', command=self.generic_callback)
+		self.empower_draw_menu.add_separator()
+		self.empower_draw_menu.add_command(label='User Defined Primitive', command=self.generic_callback)
+		self.empower_draw_menu.add_command(label='User Defined Model', command=self.generic_callback)
+		self.empower_draw_menu.add_command(label='3D Component Library', command=self.generic_callback)
+		self.empower_draw_menu.add_separator()
+		self.empower_draw_menu.add_command(label='Plane', command=self.generic_callback)
+		self.empower_draw_menu.add_command(label='Point', command=self.generic_callback)
+		self.empower_draw_menu.add_separator()
+		self.empower_draw_menu.add_command(label='Line Segment', command=self.generic_callback)
+		self.empower_draw_menu.add_separator()
+		self.empower_draw_menu.add_command(label='Region', command=self.generic_callback)
+		self.empower_draw_menu.add_command(label='SubRegion', command=self.generic_callback)
+		self.empower_menubar.add_cascade(label='Draw', menu=self.empower_draw_menu, underline=0)
 
 		# Empower Help Menu
 
@@ -153,19 +197,48 @@ class MainWindow(tk.Tk):
 		self.empower_reference_menu.add_command(label="Mie Scattering v2", command=lambda: self.open_reference("empower/help/empower/mie_v2/mie_scatter_v2.pdf"))
 
 		self.config(menu=self.menubar)
+		
+	def create_windows(self):
+		self.upper_frame = ttk.Frame(self) # Ribbon
+		self.lower_frame = ttk.Frame(self) # Display
+		self.left_frame = ttk.Frame(self.lower_frame)
+		self.right_frame = ttk.Frame(self.lower_frame)
+		self.upper_left_frame = ttk.Frame(self.left_frame) # Project Manager - Treeview
+		self.lower_left_frame = ttk.Frame(self.left_frame) # Properties - Table
+		self.upper_right_frame = ttk.Frame(self.right_frame) # Display / Work area
+		self.lower_right_frame = ttk.Frame(self.right_frame) # Status
+		
+		self.left_display_frame = ttk.Frame(self.upper_right_frame) # Object hierarchy - Treeview
+		self.right_display_frame = ttk.Frame(self.upper_right_frame) # Drawing window
+		self.left_status_frame = ttk.Frame(self.lower_right_frame) # Message Manager - Treeview
+		self.right_status_frame = ttk.Frame(self.lower_right_frame) # Progress - stacked Progressbar
+		
+		self.upper_frame.pack()
+		self.lower_frame.pack()
+		self.left_frame.pack(side=tk.LEFT)
+		self.right_frame.pack(side=tk.LEFT)
+		
+		self.upper_left_frame.pack()
+		self.lower_left_frame.pack()
+		self.upper_right_frame.pack()
+		self.lower_right_frame.pack()
+		self.left_display_frame.pack(side=tk.LEFT)
+		self.right_display_frame.pack(side=tk.LEFT)
+		self.left_status_frame.pack(side=tk.LEFT)
+		self.right_status_frame.pack(side=tk.LEFT)
 
 	def generic_callback(self):
 		pass
 		
 	def switch_window_mode(self, mode):
 		if mode == WindowMode.BASE:
-		    if self.window_mode is not WindowMode.BASE
-		        self.window_mode = WindowMode.BASE
-			    self.config(menu=self.menubar)
+			if self.window_mode is not WindowMode.BASE:
+				self.window_mode = WindowMode.BASE
+				self.config(menu=self.menubar)
 		elif mode == WindowMode.EMPOWER:
-		    if self.window_mode is not WindowMode.EMPOWER
-		        self.window_mode = WindowMode.EMPOWER
-			    self.config(menu=self.empower_menubar)
+			if self.window_mode is not WindowMode.EMPOWER:
+				self.window_mode = WindowMode.EMPOWER
+				self.config(menu=self.empower_menubar)
 			
 	def insert_empower_design(self):
 		self.switch_window_mode(mode=WindowMode.EMPOWER)
